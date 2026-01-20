88c6485c7626ca56ea3e9b8cc5d2cf4616ac579e
Egor Sapelnikov <sapelnikow.e.a@gmail.com>
2026-01-21 03:33:14 +0700

    project setup

---
+++ src/OllamaMobileClient/OllamaMobileClient.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OllamaMobileClient.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
